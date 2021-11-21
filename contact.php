8c63a1c1d9b7ee55079a7ec04078daf2375929e2
mhronyel <eng.hanif.roni63@gmail.com>
2021-11-21 20:54:14 +0600

    This is php project

---
+++ contact.php
@@ -0,0 +1,72 @@
+<!-- Contact -->
+<h1 class="">Contact Us</h1>
+<div class="contact">
+  <div class="contact_container">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12">
+          <div class="section_title text-center">
+            <div>Ciao</div>
+            <h1>Say Hello</h1>
+          </div>
+          <div class="contact_text text-center">
+            <p>Maecenas sollicitudin tincidunt maximus. Morbi tempus malesuada erat sed pellentesque. Donec pharetra mattis nulla, id laoreet neque scelerisque at. Quisque eget sem non ligula consectetur ultrices in quis augue. Donec imperd iet leo eget tortor dictum, eget varius eros sagittis.</p>
+          </div>
+        </div>
+        <div class="col-md-12">
+          <div class="contact_form_container">
+            <form action="#" class="contact_form text-center">
+              <div class="row" style="margin-top: 20px; margin-bottom: 10px;">
+                <div class="col-md-12">
+                  <div class="form-group">
+                    <div class="col-md-3">
+                      <input type="text" class="contact_input" placeholder="Your name" required="required" class="form-control">
+                    </div>
+                    <div class="col-md-3">
+                      <input type="email" class="contact_input" placeholder="Your email" required="required" class="form-control">
+                    </div>
+                    <div class="col-md-2">
+                      <input type="text" class="contact_input" placeholder="Subject" class="form-control">
+                    </div>
+                    <div class="col-md-3">
+                      <textarea class="contact_input" placeholder="Message" required="required" class="form-control"></textarea>
+                    </div>
+                    <div class="col-md-1">
+                      <button class="contact_button" class="btn btn-outline-success">send message</button>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <!-- Map -->
+  <div class="contact_map_container">
+    <div class="map">
+      <div id="google_map" class="google_map">
+        <div class="map_container">
+          <div id="map"></div>
+        </div>
+      </div>
+    </div>
+
+    <!-- Contact Map Content -->
+    <!-- <div class="contact_map_content">
+      <div class="d-flex flex-column align-items-center justify-content-center">
+        <img class="contact_info_logo_1" src="images/logo_4.png" alt="">
+        <img class="contact_info_logo_2" src="images/logo_2.png" alt="">
+        <div class="contact_info_list">
+          <ul class="text-center">
+            <li>13,road:23, kuya kata</li>
+            <li>admin@kuyakata.com</li>
+            <li>01734382806</li>
+          </ul>
+        </div>
+      </div>
+    </div> -->
+  </div>
+</div>
